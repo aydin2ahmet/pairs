e04aad58eaf1b54a456afc32390d6187a195ad3e
AHMET AYDIN <ahmet.aydin@visiomex.com>
2022-06-20 18:54:34 +0300

    solving commit

---
+++ Pairs/Program.cs
@@ -0,0 +1,22 @@
+﻿var result = pairs(2, new List<int>() { 1, 5, 3, 4, 2 });
+
+Console.WriteLine(result);
+
+int pairs(int k, List<int> arr)
+{
+    Dictionary<int, bool> valuesDict = new Dictionary<int, bool>();
+    int count = 0;
+
+    foreach (var i in arr)
+    {
+        if (valuesDict.ContainsKey(i - k))
+            count += 1;
+
+        if (valuesDict.ContainsKey(i + k))
+            count += 1;
+
+        valuesDict[i] = true;
+    }
+
+    return count;
+}
